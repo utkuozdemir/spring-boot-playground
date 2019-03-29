4950139b91c3f437023e1ef0a5f2c3f6dfe9300d
Utku Özdemir <utku@mobilabsolutions.com>
2019-03-29 13:32:05 +0100

    initial commit

---
+++ src/main/kotlin/org/utkuozdemir/springbootplayground/SpringBootPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package org.utkuozdemir.springbootplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootPlaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootPlaygroundApplication>(*args)
+}
---
+++ src/test/kotlin/org/utkuozdemir/springbootplayground/SpringBootPlaygroundApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.utkuozdemir.springbootplayground
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringBootPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
